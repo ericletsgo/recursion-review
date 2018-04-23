8ca2f72ef84da3d3a0a2825f262c37ef9e947ce5
ericletsgo <ericletsgo@gmail.com>
2018-04-23 16:28:24 -0700

    pseudocode and first iteration for getting elements by class name

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,24 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+
+// You should use document.body, element.childNodes, and element.classList
+
+var getElementsByClassName = function(className) {
+  // input: array
+  // output: array of elements that have the class names
+  // for each string in the array,
+    // if array index contains className,
+      // push that index to resultArray
+  var nodes = document.body;
+  var resultArray = [];
+
+  for (var i = 0; i < nodes.length; i++) {
+    if (nodes[i].classList.contains(className)) {
+      resultArray.push(nodes[i]);
+    }
+    
+  }
+
+  return resultArray;
 };
