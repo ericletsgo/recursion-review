76e446369aa17ad823329590b4319846db704a25
ericletsgo <ericletsgo@gmail.com>
2018-04-23 15:55:06 -0700

    solved recursive stringify for primitives, arrays, and objects

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,55 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+  // this is for numbers
+  // if obj is num then return empty + obj
+  var resultArray = [];
+
+  if (typeof obj === 'function' || typeof obj === 'undefined') {
+    return '{}';
+  }
+
+  if (typeof obj === 'number' || typeof obj === 'boolean' || obj === null) {
+    return '' + obj;  
+  }
+  // this for strings
+  if (typeof obj === 'string') {
+    return '"' + obj + '"';
+  }
+  // this is for arrays
+  if (Array.isArray(obj)) {
+  
+  // for each ele of the arr,
+    obj.forEach(function(ele) {
+      resultArray.push(stringifyJSON(ele));
+    });
+  // stringify the ele,
+  // push stringified ele to empty array,
+  // return stringified array 
+  return '[' + resultArray + ']';
+  }
+  // this is for objects
+  if (typeof obj === 'object') {
+    if (Object.keys(obj).length === 0) {
+      return '{}';
+    } 
+
+    for (var key in obj) {
+      if (typeof obj[key] === 'function' || obj[key] === undefined) {
+        return '{}';
+      }
+      resultArray.push(stringifyJSON(key) + ':' + stringifyJSON(obj[key]));
+    }
+
+    return '{' + resultArray + '}';
+  // for each key in the obj
+  // stringify key
+  // declare stringified key to equal stringified value
+  // return resultObj
+    // for (var key in obj) {
+
+
+    // }
+  }
+ 
 };
